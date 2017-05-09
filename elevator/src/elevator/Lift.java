dabd3202c57e6b4321bb73de3a969cea957fb35d
Grant Worsley <grant.worsley@capgemini.com>
2017-05-09 10:26:50 +0100

    Altered initialised variables and commented code containing start of updateDestinations method

--- elevator/src/elevator/Lift.java
+++ elevator/src/elevator/Lift.java
@@ -1,47 +1,73 @@
 package elevator;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Lift {
 	//current floor that the elevator is on
-	public int currentFloor;
+	private int currentFloor;
 	
 	//Direction is a boolean, with True meaning up and False meaning down
-	public boolean currentDirection;
+	private boolean currentDirection;
 	
 	//Array containing the people in the elevator
-	public ArrayList<Person> contains;
+	private static LinkedHashMap<Person, Boolean> contains = contains = new LinkedHashMap<Person, Boolean>();
 	
 	//Array containing the floors that the people in the elevator want to go to
 	//This is static as when an elevator is called the name will not always be known to the person class
-	public static ArrayList<Floor> destinations = new ArrayList<Floor>();
+	private ArrayList<Integer> destinations;
 	
 	public Lift(){
 		currentFloor = 0;
 		currentDirection = true;
-		contains = new ArrayList<Person>();
+		destinations = new ArrayList<Integer>();
 	}
 	
-	public void open(){
+	private void open(){
 		
 	}
 	
-	public void close(){
+	private void close(){
 		
 	}
 	
-	public void moveUp(){
+	private void moveUp(){
 		
 	}
 	
-	public void moveDown(){
+	private void moveDown(){
 		
 	}
 	
-	public void stopAtFloor(int floor){
+	private void stopAtFloor(int floor){
 		if (floor != currentFloor){
 			
 		}
 	}
+	
+	//Gets passed the person when they call the lift as to know who they are and where they want to go
+	public static void addDestination(Person person){
+		contains.put(person, false);
+	}
+	
+	//Populates an array of destinations 
+/*	private void updateDestinations(){
+		ArrayList<Integer> otherDestinations = new ArrayList<Integer>();
+		if (currentDirection){
+			for (Person person : contains.keySet()){
+				if (person.getDestination() > currentFloor){
+					destinations.add(person.getDestination());
+				}else{
+					
+				}
+			}
+		}else{
+			for (Person person : contains.keySet()){
+				if (person.getDestination() < currentFloor){
+					destinations.add(person.getDestination());
+				}
+			}
+		}
+	}*/
 
 }
