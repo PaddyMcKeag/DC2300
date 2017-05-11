4638f76003bedef98d5ed914fcb96339d03d66b0
Grant Worsley <grant.worsley@capgemini.com>
2017-05-11 12:13:23 +0100

    added seed value to employee variable declarations in test class

--- elevator/src/test/LiftTest.java
+++ elevator/src/test/LiftTest.java
@@ -17,15 +17,15 @@ public class LiftTest {
 		//creates building
 		Building building = new Building(8);
 		//Creates employees
-		Employee employee1 = new Employee(1);
-		Employee employee2 = new Employee(2);
-		Employee employee3 = new Employee(3);
-		Employee employee4 = new Employee(4);
-		Employee employee5 = new Employee(5);
-		Employee employee6 = new Employee(6);
-		Employee employee7 = new Employee(7);
-		Employee employee8 = new Employee(8);
-		Employee employee9 = new Employee(9);
+		Employee employee1 = new Employee(1, 1);
+		Employee employee2 = new Employee(2, 2);
+		Employee employee3 = new Employee(3, 3);
+		Employee employee4 = new Employee(4, 4);
+		Employee employee5 = new Employee(5, 5);
+		Employee employee6 = new Employee(6, 6);
+		Employee employee7 = new Employee(7, 7);
+		Employee employee8 = new Employee(8, 8);
+		Employee employee9 = new Employee(9, 9);
 		//Changes employees current floor
 		employee1.setCurrentFloor(1);
 		employee2.setCurrentFloor(3);
